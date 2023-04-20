d0ddd48edcec2e02bcae1f8e316647fe2e2223f3
MinnaDeVerdier <minna.deverdier@gmail.com>
2023-04-20 19:38:20 +0200

    fungerande highscore

--- HighScoreEntry.cs
+++ HighScoreEntry.cs
@@ -9,57 +9,104 @@ namespace Breakout
 {
     public class HighScoreEntry
     {
+        // Highscore-listan skapas
+        static HighScoreEntry topThree = new();
+        public HighScoreEntry TopThree { get => topThree; set => topThree = value; }
 
-        //public int HSScore;
-        static string dir = "..\\..\\..\\";
-        static string path = Path.GetFullPath(dir);
-        int[] hsScore = new int[3] { 1, 10, 100 };
-        string[] name = new string[3] {"NN","NN","NN"};
+        // Listans innehåll från start
+        int[] hsScore = new int[3];
+        string[] name = new string[3];
+        public int[] HsScore { get => hsScore; set => hsScore = value; }
+        public string[] Name { get => name; set => name = value; }
 
-        public HighScoreEntry(string contents, int count)
-        {
-            string[] field = contents.Split(",");
-            name[count] = field[count];
-            hsScore[count] = Convert.ToInt32(field[count++]);
-        }
-     
-        public static List<HighScoreEntry> highScoreEntries = new();
+
+        // Hittar sparfilen 
+        static string path = Path.GetFullPath("..\\..\\..\\");
+        static string hsFile = path + "HSlist.txt";
+
+        // Läser in highscore från sparfilen
         public static void ReadHighScoreFromFile()
         {
             string line;
             int counter = 0;
-            using (StreamReader file = new(path+"HSlist.txt"))
+            using (StreamReader file = new(hsFile))
             {
                 while ((line = file.ReadLine()) != null)
                 {
-                    HighScoreEntry addition = new(line, counter);
+                    string[] field = line.Split(",");
+                    topThree.name[counter] = field[0];
+                    topThree.hsScore[counter] = int.Parse(field[1]);
                     counter++;
                 }
             }
         }
-        static void EnterHighScore() 
-        { 
-            Write("Congratulations! \nYou reached our top three highest scores, well played. \nEnter your name: \n> ");
-            string name = ReadLine();
 
-        }
-        public static void AddHighScoreToFile(string inpName, int newHScore)
+        // Kollar och möblerar om toppliste-placeringen
+        public static void CheckPlacement()
         {
-            string input = inpName+","+newHScore.ToString();
-            using (StreamWriter file = new("..\\..\\..\\HSlist.txt", true))
+            int score = Program.PreviousScore;
+            if (score < topThree.hsScore[2])
+                return;
+            else if (score >= topThree.hsScore[0])
             {
-                file.WriteLine(inpName + "," + newHScore);
+                topThree.hsScore[2] = topThree.hsScore[1];
+                topThree.name[2] = topThree.name[1];
+                topThree.hsScore[1] = topThree.hsScore[0];
+                topThree.name[1] = topThree.name[0];
+                EnterHighScore(0);
+            }
+            else if (score >= topThree.hsScore[1])
+            {
+                topThree.hsScore[2] = topThree.hsScore[1];
+                EnterHighScore(1);
+            }
+            else if (score >= topThree.hsScore[2])
+            {
+                EnterHighScore(2);
+            }
+         
+        }
+
+        // Om poängen når topplistan
+        static void EnterHighScore(int place)
+        {
+            Write("Congratulations! \nYou reached our top three highest scores, well played. \nEnter your name: \n> ");
+            topThree.name[place] = ReadLine();
+            topThree.hsScore[place] = Program.PreviousScore;
+            Write("Temporarily added to Highscore.\nSave highscore list permanently? (y/n) ");
+            string ans = ReadLine();
+            if (char.ToLower(ans[0]) == 'y')
+            {
+                AddHighScoreToFile();
+                WriteLine("Saved.");
+                ReadKey(true);
+            }
+            else
+            {
+                WriteLine("Highscore will revert to last save when ending program.");
+                ReadKey(true);
+            }
+            Clear();
+            return;
+        }
+
+        // Skriver över sparfilen med nya topp tre
+        public static void AddHighScoreToFile()
+        {
+            using (StreamWriter file = new(hsFile))
+            {
+                for (int i = 0; i < topThree.hsScore.Length; i++)
+                {
+                    file.WriteLine(topThree.name[i] + "," + topThree.hsScore[i]);
+                }
             }
         }
         public static void PrintList()
         {
-            WriteLine(path);
-    /*        highScoreEntries.Sort((x, y) => y.HSScore.CompareTo(x.HSScore));
-            foreach (HighScoreEntry highScoreEntry in highScoreEntries)
+            for (int i = 0; i < 3; i++)
             {
-                WriteLine($"{highScoreEntry.Name}, {highScoreEntry.HSScore} ");
+                WriteLine($"   ~~~~  {i + 1} place  ~~~~  \n {topThree.name[i],-10}  ::  {topThree.hsScore[i]} points.\n");
             }
-         */   //highScoreEntries.Clear();
         }
     }
 
